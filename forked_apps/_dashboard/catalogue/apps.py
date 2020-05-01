d0c297c99e657d07109c93aaebf4a4973543bcb5
Geoffroy Blondel <geoffroy.blondel@gmail.com>
2020-05-01 19:29:56 +0200

    delete

--- forked_apps/_dashboard/catalogue/apps.py
+++
@@ -1,5 +0,0 @@
-import oscar.apps.dashboard.catalogue.apps as apps
-
-
-class CatalogueDashboardConfig(apps.CatalogueDashboardConfig):
-    name = 'forked_apps.dashboard.catalogue'
--- forked_apps/_dashboard/catalogue/forms.py
+++
@@ -1,9 +0,0 @@
-#-*- coding: utf-8 -*-
-from uploads.core.models import Document
-from django import forms
-
-
-class CsvForm(forms.ModelForm):
-    class Meta:
-        model = Document
-        fields = ('description', 'document', )
