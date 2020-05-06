aa5c02e58e0777d6ace9785cf0a07dfdf2c5d67c
Geoffroy Blondel <geoffroy.blondel@gmail.com>
2020-05-06 11:39:43 +0200

    add structure field to order model

--- apps_fork/order/models.py
+++ apps_fork/order/models.py
@@ -1,1 +1,23 @@
+from oscar.apps.order.abstract_models import AbstractOrder
+from django.utils.translation import gettext_lazy as _
+from django.db import models
+
+class Order(AbstractOrder):
+
+    # add structure field
+    # STANDALONE -> only 1 partner
+    # PARENT -> multi-partner, client view
+    # CHILD -> mutli-partner, partner view
+
+    STANDALONE, PARENT, CHILD = 'standalone', 'parent', 'child'
+    STRUCTURE_CHOICES = (
+        (STANDALONE, _('Stand-alone order')),
+        (PARENT, _('Parent order')),
+        (CHILD, _('Child order'))
+    )
+    structure = models.CharField(
+        _("Order structure"), max_length=10, choices=STRUCTURE_CHOICES,
+        default=STANDALONE)
+
+
 from oscar.apps.order.models import *  # noqa isort:skip
