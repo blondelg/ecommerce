558be0b619827ba14ef16d77ec6f9abb5adb185c
Geoffroy Blondel <geoffroy.blondel@gmail.com>
2020-07-30 09:40:38 +0200

    update donation view, add totals

--- apps_fork/dashboard/donation/views.py
+++ apps_fork/dashboard/donation/views.py
@@ -7,6 +7,7 @@ from oscar.core.loading import get_class, get_model
 
 
 Donation = get_model('order', 'Donation')
+Project = get_model('blog', 'BlogProjet')
 
 
 class DonationListView(generic.ListView):
@@ -17,16 +18,42 @@ class DonationListView(generic.ListView):
     def get_queryset(self):
 
         if self.request.user.is_staff:
-            return [Donation.objects.all(), Donation.objects.all()]
+            return Donation.objects.all()
             
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
-        data['page_title'] = 'Authors'
+        data['df'] = self.get_project_progress().to_html(justify='left', index=False, classes='table table-striped table-bordered')
         return data
         
     def get_project_progress(self):
         """ for each project, calculates sum of donations """
-        pass
+        
+        # get data
+        donations = Donation.objects.all()
+        projects = Project.objects.all()
 
+        # queryset to dataframe
+        df_donations = pd.DataFrame(list(donations.values()))
+        df_projects = pd.DataFrame(list(projects.values()))[['title', 'target']]
+        
+        # left join
+        df = df_donations.merge(df_projects, how='left', left_on='project_name', right_on='title')[['project_name', 'asso_name', 'target', 'amount']]
+        
+        # calculate sums
+        df = df.groupby(by=['project_name', 'asso_name', 'target'], axis=0, as_index=False).sum()
+        
+        # calculate progress
+        df['progress'] = df.apply(lambda x: (x['amount'] / x['target']) * 100, axis=1)
+        
+        # rename headers
+        df.columns = ['Project', 'Association', 'Target', 'Total', 'Progress']
+        
+        # update columns formats
+        df['Target'] = df['Target'].apply(lambda x: str(x) + " €")
+        df['Total'] = df['Total'].apply(lambda x: str(x) + " €")
+        df['Progress'] = df['Progress'].apply(lambda x: str(round(x, 2)) + " %")
+
+        
+        return df
 
 
