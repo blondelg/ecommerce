ef751118a1b9bfa280c16ca59de866920b47d61e
Geoffroy Blondel <geoffroy.blondel@gmail.com>
2020-08-02 13:10:59 +0200

    addshipping options to basket

--- apps_fork/dashboard/donation/views.py
+++ apps_fork/dashboard/donation/views.py
@@ -1,8 +1,10 @@
 from django.views import generic
 from django.shortcuts import render
+from django.conf import settings
 import pandas as pd
 
 from oscar.core.loading import get_class, get_model
+from oscar.views.generic import BulkEditMixin
 
 
 
@@ -10,15 +12,16 @@ Donation = get_model('order', 'Donation')
 Project = get_model('blog', 'BlogProjet')
 
 
-class DonationListView(generic.ListView):
+class DonationListView(BulkEditMixin, generic.ListView):
     model = Donation
     template_name = "oscar/dashboard/donation/donation_list.html"
     context_object_name = "donations"
+    paginate_by = settings.OSCAR_DASHBOARD_ITEMS_PER_PAGE
 
     def get_queryset(self):
 
         if self.request.user.is_staff:
-            return Donation.objects.all()
+            return Donation.objects.get_queryset().order_by('-pk')
             
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
@@ -55,5 +58,8 @@ class DonationListView(generic.ListView):
 
         
         return df
+        
+    def get_paginate_by(self, queryset):
+        return self.paginate_by
 
 
