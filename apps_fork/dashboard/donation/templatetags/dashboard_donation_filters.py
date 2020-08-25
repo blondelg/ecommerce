31aa3681995251608d0ea0b96e62a33a7062cef4
Geoffroy Blondel <geoffroy.blondel@gmail.com>
2020-08-25 14:30:38 +0200

    setup progress bar for projects

--- apps_fork/dashboard/donation/templatetags/dashboard_donation_filters.py
+++ apps_fork/dashboard/donation/templatetags/dashboard_donation_filters.py
@@ -6,3 +6,8 @@ register = template.Library()
 def percent(value):
     """returns percentage"""
     return round(value * 100, 2)
+    
+@register.filter(name='percent_no_digit')
+def percent_no_digit(value):
+    """returns percentage"""
+    return round(value * 100)
