550c087bd2ace8bf88c5d920e1c196c88b2fcf18
Geoffroy Blondel <geoffroy.blondel@gmail.com>
2020-08-29 19:33:34 +0200

    add content project model to admin

--- apps_custom/content/admin.py
+++ apps_custom/content/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib import admin
+from oscar.core.loading import get_model
+from oscar.apps.order.admin import *  # noqa
 
-# Register your models here.
+ContentProjet = get_model('content', 'ContentProjet')
+
+class ContentProjetAdmin(admin.ModelAdmin):
+    model = ContentProjet
+    list_display = ('target', 'asso', 'achievement', 'achievement_percent', 'achieved', 'donation_count')
+    
+admin.site.register(ContentProjet, ContentProjetAdmin)
+
